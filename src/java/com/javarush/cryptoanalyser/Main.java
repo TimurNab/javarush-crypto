3a241fb86da40919b5ead6880251ba1e1d8fdfaf
Timur Nabiev <tnabiev82@gmail.com>
2022-03-17 11:46:56 +0500

    Added decrypt method and changed encryption algorithm.

--- src/java/com/javarush/cryptoanalyser/Main.java
+++ src/java/com/javarush/cryptoanalyser/Main.java
@@ -2,13 +2,8 @@ package com.javarush.cryptoanalyser;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        Crypto crypto = new Crypto();
-
-        for (int i = 0; i < Crypto.ints.length; i++) {
-
-            System.out.println((char) Crypto.ints[i]);
-        }
+        System.out.println(Crypto.enCode("Привет, как дела? Пойдем гулять.", 5));
+        System.out.println(Crypto.deCode("Фхнжйч:ДпепДийреГДФуоийсДзшр\"ч,'", 5));
     }
 }
